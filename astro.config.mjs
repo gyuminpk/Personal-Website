982e12fdbe82c9ec4428eff4adfffd04bc0b7c04
gyuminpk <gmin.park22@gmail.com>
2024-08-04 11:01:59 +0900

    Enable viewTransitions experimental feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    viewTransitions: true
+  },
   site: 'https://gyuminpk.github.io',
-  base: 'Personal-Website',
+  base: 'Personal-Website'
 });
