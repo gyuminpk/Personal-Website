4ff84147d133e57dd3202a52c0d17e1d495d830e
gyuminpk <gmin.park22@gmail.com>
2024-08-04 11:11:10 +0900

    Fix @astrojs/assets integration import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import astroAssets from '@astrojs/assets';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
